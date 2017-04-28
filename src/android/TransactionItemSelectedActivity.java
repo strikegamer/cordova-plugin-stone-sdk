56a2418ac637aba3ffd944be6d10a298e9d6bcb5
Unknown <joaovictor_mendes@hotmail.com>
2017-04-28 14:17:01 -0300

    Removing Print

--- src/android/TransactionItemSelectedActivity.java
+++ src/android/TransactionItemSelectedActivity.java
@@ -28,36 +28,7 @@ public class TransactionItemSelectedActivity extends CordovaPlugin {
                 String optSelected = data.getString(1);
                 System.out.println("optSelected: " + optSelected);
 
-                if(btnClicked.equals("PRINT")) {
-                    System.out.println("Opção Selecionada Print");
-
-                    try {
-                        // lógica da impressão
-                        List<PrintObject> listToPrint = new ArrayList<PrintObject>();
-                        for (int i = 0; i < 10; i++) {
-                            listToPrint.add(new PrintObject("Teste de impressão linha " + i, PrintObject.MEDIUM, PrintObject.CENTER));
-                        }
-                        // GlobalInformations.getPinpadFromListAt(0) eh o pinpad conectado, que esta na posicao zero.
-                        final PrintProvider printProvider = new PrintProvider(TransactionItemSelectedActivity.this.cordova.getActivity(), listToPrint, GlobalInformations.getPinpadFromListAt(0));
-                        printProvider.setWorkInBackground(false);
-                        printProvider.setDialogMessage("Imprimindo...");
-                        printProvider.setConnectionCallback(new StoneCallbackInterface() {
-                            public void onSuccess() {
-                                Toast.makeText(TransactionItemSelectedActivity.this.cordova.getActivity(), "Impressão realizada com sucesso", Toast.LENGTH_SHORT).show();
-                            }
-
-                            public void onError() {
-                                Toast.makeText(TransactionItemSelectedActivity.this.cordova.getActivity(), "Um erro ocorreu durante a impressão", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                        printProvider.execute();
-                    } catch (IndexOutOfBoundsException outException) {
-                        Toast.makeText(TransactionItemSelectedActivity.this.cordova.getActivity(), "Conecte-se a um pinpad.", Toast.LENGTH_SHORT).show();
-                    } catch (Exception e) {
-                        Toast.makeText(TransactionItemSelectedActivity.this.cordova.getActivity(), "Houve um erro inesperado. Tente novamente mais tarde.", Toast.LENGTH_SHORT).show();
-                        e.printStackTrace();
-                    }
-                } else if(btnClicked.equals("CANCEL")) {
+                if(btnClicked.equals("CANCEL")) {
                     System.out.println("Opção Selecionada Cancel");
 
                     // Pega o id da transação selecionada.
