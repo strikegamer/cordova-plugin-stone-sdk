960222563d7dffe333c45d010ff9dd52e6684713
Unknown <joaovictor_mendes@hotmail.com>
2017-05-02 17:11:15 -0300

    v0.2.0

---
+++ www/stone_sdk.js
@@ -0,0 +1,20 @@
+module.exports = {
+  device: function (successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "StoneSDK", "device", []);
+  },
+  deviceSelected: function (arrayList, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "StoneSDK", "deviceSelected", [arrayList]);
+  },
+  transaction: function (amount, method, instalments, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "StoneSDK", "transaction", [amount, method, instalments]);
+  },
+  transactionCancel: function (transactionCode, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "StoneSDK", "transactionCancel", [transactionCode]);
+  },
+  transactionList: function (successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "StoneSDK", "transactionList", []);
+  },
+  validation: function (stoneCodeList, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "StoneSDK", "validation", [stoneCodeList]);
+  }
+};
